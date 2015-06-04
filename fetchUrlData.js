1c83a2253b1cb9a69987bd1bbc75c43a2ebf1261
kavedder <katie.vedder@socrata.com>
2015-06-03 20:48:25 -0700

    hurray

--- fetchUrlData.js
+++ fetchUrlData.js
@@ -10,7 +10,6 @@ function getStopLoc(doc) {
     var wordConvert = {
         "Ln": "Lane",
         "St": "Street",
-        "St.": "Street",
         "Blvd": "Boulevard",
         "Ave": "Avenue",
         "Wy": "Way",
@@ -24,16 +23,36 @@ function getStopLoc(doc) {
         "W": "west",
         "S": "south",
         "E": "east",
-        "Rd": "Road"
-    }
+        "Mt": "Mount",
+        "Ln.": "Lane",
+        "St.": "Street",
+        "Blvd.": "Boulevard",
+        "Ave.": "Avenue",
+        "Wy.": "Way",
+        "Pkwy.": "Parkway",
+        "Rd.": "Road",
+        "NE.": "northeast",
+        "NW.": "northwest",
+        "SE.": "southeast",
+        "SW.": "southwest",
+        "N.": "north",
+        "W.": "west",
+        "S.": "south",
+        "E.": "east",
+        "Mt.": "Mount"
+    };
+
     // well, this is fragile...
     var stop = doc.getElementsByTagName("entry").item(0).getElementsByTagName("name").item(0).firstChild.data;
     var stop_words = stop.split(" ");
     var converted = "";
     for (var i=0; i<stop_words.length; i++) {
-        word = stop_words[i]
+        word = stop_words[i];
         convertedWord = wordConvert[word] || word;
         converted += " " + convertedWord;
     }
-    return converted
+    var direction = doc.getElementsByTagName("entry").item(0).getElementsByTagName("direction").item(0).firstChild.data;
+    direction = wordConvert[direction.trim()];
+
+    return converted + " " + direction + "bound ";
 }
--- getRoutes.js
+++ getRoutes.js
@@ -1,35 +1,28 @@
-function getRoutes(doc, bus_id, spoken_form) {
-    // TODO: the "all" version!
+function getRoutes(doc, bus_id, bus_type) {
     var routes = doc.getElementsByTagName("arrivalAndDeparture");
-    var arr = [];
+    var timeArray = [];
+    var routeArray = [];
     var longName;
     var destination;
     var shortName;
     var arrTime;
     for (i=0; i<routes.length; i++) {
         var element = routes.item(i);
-        try {
             shortName = element.getElementsByTagName("routeShortName").item(0).firstChild.data;
-        }
-        catch(err) {
-            shortName = spoken_form;
-        }
-        if (shortName == bus_id) {
-            if (!destination) {
-                // haven't grabbed the destination yet
-                try {
-                    longName = element.getElementsByTagName("routeLongName").item(0).firstChild.data;
-                }
-                catch(err) {
-                    longName = makeLong(spoken_form);
-                }
-
-                try {
-                    destination = " to " + element.getElementsByTagName("tripHeadsign").item(0).firstChild.data;
-                }
-                catch(err) {
-                    destination = " ";
-                }
+        if (shortName == bus_id || bus_type == "all") {
+            // gather the name info
+            try {
+                longName = element.getElementsByTagName("routeLongName").item(0).firstChild.data;
+            }
+            catch(err) {
+                // it's a number!
+                longName = makeLong(shortName);
+            }
+            try {
+                destination = " to " + cvtDest(element.getElementsByTagName("tripHeadsign").item(0).firstChild.data);
+            }
+            catch(err) {
+                destination = " ";
             }
             //predictedArrivalTime
             try {
@@ -40,36 +33,114 @@ function getRoutes(doc, bus_id, spoken_form) {
             }
             var time = getTimeDiff(arrTime);
             if (time) {
-                arr.push(time);
+                timeArray.push(time);
+                routeArray.push(longName);
             }
         }
     }
-    if (arr.length === 0) {
-        return ("I'm sorry. I can't find any upcoming arrivals of the " + bus_id + " at this stop.");
-    }
-    var s = "The next arrival of the " + longName + destination + " is  in about " + arr[0] + ". ";
-    if (arr.length > 1) {
-        if (arr.length > 2) {
-            s += "After that, the next arrivals are ";
-            for (var i=1; i<arr.length-1; i++) {
-                s += " in about " + arr[i] + ", ";
+    // syntaxing
+    var return_str = "";
+    if (bus_type == "all") {
+        // syntax for all buses
+        if (timeArray.length === 0) {
+            return "I'm sorry. I can't find any upcoming arrivals at this stop.";
+        }
+        return_str = "The next arrival at this stop is the " + routeArray[0] + " in about " + timeArray[0] + ", ";
+        if (timeArray.length > 1) {
+            return_str += "followed by ";
+            for (var i=1; i<timeArray.length; i++) {
+                return_str += " the " + routeArray[i] + " in about " + timeArray[i] + ", ";
             }
-            s += " and in about " + arr[arr.length-1];
-        }
-        else {
-            s += "After that, the next arrival is in about " + arr[arr.length-1];
         }
     }
-    return s;
+    else {
+        // syntax for a single bus
+        if (timeArray.length === 0) {
+            return "I'm sorry. I can't find any upcoming arrivals for the " + bus_id + " at this stop.";
+        }
+        return_str = "The next arrival of the " + longName + destination + " is  in about " + timeArray[0] + ". ";
+        if (timeArray.length > 1) {
+            if (timeArray.length > 2) {
+                return_str += "After that, the next arrivals are ";
+                for (var i=1; i<timeArray.length-1; i++) {
+                    return_str += " in about " + timeArray[i] + ", ";
+                }
+                return_str += " and in about " + timeArray[arr.length-1];
+            }
+            else {
+                return_str += "After that, the next arrival is in about " + timeArray[timeArray.length-1];
+            }
+        }
+    }
+    return_str += ". ";
+    return return_str;
 }
 
-function makeLong(spoken_form) {
-    var substrings = spoken_form.split(" ");
-    var first_word = substrings[0];
-    if (first_word != "the" ) {
-        substrings.unshift("the");
+
+function cvtDest(dest) {
+    var wordConvert = {
+        "Ln": "Lane",
+        "St": "Street",
+        "Blvd": "Boulevard",
+        "Ave": "Avenue",
+        "Wy": "Way",
+        "Pkwy": "Parkway",
+        "Rd": "Road",
+        "NE": "northeast",
+        "NW": "northwest",
+        "SE": "southeast",
+        "SW": "southwest",
+        "N": "north",
+        "W": "west",
+        "S": "south",
+        "E": "east",
+        "Mt": "Mount",
+        "Ln.": "Lane",
+        "St.": "Street",
+        "Blvd.": "Boulevard",
+        "Ave.": "Avenue",
+        "Wy.": "Way",
+        "Pkwy.": "Parkway",
+        "Rd.": "Road",
+        "NE.": "northeast",
+        "NW.": "northwest",
+        "SE.": "southeast",
+        "SW.": "southwest",
+        "N.": "north",
+        "W.": "west",
+        "S.": "south",
+        "E.": "east",
+        "Mt.": "Mount"
+    };
+    var words = dest.split(" ");
+    var converted = "";
+    for (var i=0; i<words.length; i++) {
+        word = words[i];
+        convertedWord = wordConvert[word] || word;
+        converted += " " + convertedWord;
     }
-    return substrings.join(" ");
+    return converted;
+}
+
+function makeLong(busno) {
+    // just in case?
+    var chars = busno.toString().trim().split('');
+    var longno;
+    if (chars.length == 4) {
+        longno = chars[0]+chars[1]+" "+chars[2]+chars[3];
+    }
+    else if (chars.length == 3) {
+        longno = chars[0]+" "+chars[1]+chars[2];
+    }
+    else {
+        longno=chars[0]+chars[1];
+    }
+    var minus_2 = longno.length - 2;
+    var minus_1 = minus_2 + 1;
+    if (longno.slice(minus_2,minus_1) == '0') {
+        longno = longno.slice(0,minus_2) + "oh " + longno.slice(minus_1);
+    }
+    return longno;
 }
 
 function getTimeDiff(timestr) {
