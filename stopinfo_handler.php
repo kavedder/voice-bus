cf09432e275afd943de10329c6b751323ea4ab9f
Katie Vedder <kvedder@gmail.com>
2015-06-08 18:51:11 -0700

    tweak php

--- stopinfo_handler.php
+++ stopinfo_handler.php
@@ -8,7 +8,6 @@ $url = "http://stopinfo.pugetsound.onebusaway.org/busstops/1_" . $stopno;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 $html = curl_exec($ch);
 curl_close($ch);
 
