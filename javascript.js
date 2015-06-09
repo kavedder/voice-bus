9554d4a64e8f949a61cfefc88a55e6881d391616
Katie Vedder <kvedder@gmail.com>
2015-06-08 18:48:33 -0700

    removing cached stopinfo; add php stopinfo

--- javascript.js
+++ javascript.js
@@ -73,13 +73,14 @@ var lineLetters = [
     "A", "B", "C", "D", "E", "F"
 ];
 
-function convertBus(str, bus_type) {
+function convertNumFromStr(str, bus_type) {
     // "two hundred" => 200
     // "the rapid ride A" => A Line
     // "any bus" => null
-    // if it came from DTMF, just remove all the spaces
-    var substrings;
+
+    var substrings = str.split(" ");
     var substring;
+
     if (bus_type == "all") {
         return null;
     }
@@ -88,7 +89,6 @@ function convertBus(str, bus_type) {
         // but we had to come here to convert numbers,
         // anyway, and this way we don't have to have
         // so many crazy return values
-        substrings = str.split(" ");
         var lineLetter;
         for (var i=0; i<substrings.length; i++) {
             substring = substrings[i];
@@ -100,47 +100,56 @@ function convertBus(str, bus_type) {
         return lineLetter + " Line";
     }
     else {
-        substrings = str.split(" ");
-        var converted="";
-        if (isNaN(substrings[0])) {
-            var last_substring;
-            var has_hundred=false;
-            for (var i=0; i<substrings.length; i++) {
-                substring = substrings[i];
-                var converted_substring = numConvert[substring];
-                converted += converted_substring;
-                last_substring=substring;
-                if (substring == "hundred") {
-                    has_hundred = true;
-                }
+        var converted=[];
+        var last_substring;
+        var has_hundred=false;
+        for (var i=0; i<substrings.length; i++) {
+            substring = substrings[i];
+            var converted_substring = numConvert[substring];
+            converted.push(converted_substring);
+            last_substring=substring;
+            if (substring == "hundred") {
+                has_hundred = true;
             }
-            if (last_substring.slice(-2) == "ty") {
-                // ended in eg. forty; add a zero
-                converted += "0";
-            }
-            if (last_substring == "hundred") {
-                // eg. five hundred
-                converted += "00";
-            }
-            else if (has_hundred && converted.length == 2) {
-                // didn't end with hundred, but had it in the middle
-                // and the string is too short
-                converted = converted[0] + "0" + converted[1];
-            }
-            // just in case of spacey errors
-            converted = converted.replace("undefined", "");
         }
-        else {
-            for (var i=0; i<substrings.length; i++) {
-                substring = substrings[i];
-                if (substring != "#") {
-                    converted += substring;
-                }
-            }
-            return converted;
+        if (last_substring.slice(-2) == "ty") {
+            // ended in eg. forty; add a zero
+            converted.push("0");
         }
+        if (last_substring == "hundred") {
+            // eg. five hundred
+            converted.push("00");
+        }
+        else if (has_hundred && converted.length == 2) {
+            // didn't end with hundred, but had it in the middle
+            // and the string is too short
+            converted = [converted[0], "0", converted[1]];
+        }
+        // just in case of spacey errors
+        converted = converted.join("");
+        converted = converted.replace("undefined", "");
+        return converted;
     }
 }
+
+function convertNumFromDTMF(num) {
+    var substrings = num.split(" ");
+    var substring;
+    var converted = "";
+    for (var i=0; i<substrings.length; i++) {
+        substring = substrings[i];
+        if (substring == "*") {
+            return null;
+        }
+        if (substring != "#") {
+            converted += substring;
+        }
+    }
+    return converted;
+}
+
+
+
 
 function getStatus(doc) {
     var response = doc.getElementsByTagName("text").item(0).firstChild.data;
